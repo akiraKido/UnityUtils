31739721a381114990ce30a3e202dc11d0100e8c
Akira Kido <achts.y@gmail.com>
2020-08-22 21:36:11 +0900

    initial commit

---
+++ Assets/Scripts/UnityUtils.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityUtils
+{
+    public interface IInitializable<in T>
+    {
+        void Initialize(T value);
+    }
+
+    public static class InitializableExtensions
+    {
+        public static TInstance Instantiate<TInstance, TValue>(this TInstance initializable, TValue value)
+            where TInstance : MonoBehaviour, IInitializable<TValue>
+        {
+            var obj = Object.Instantiate(initializable);
+            obj.Initialize(value);
+            return obj;
+        }
+    }
+}
